130fad781d62bf3665535e07a71f89cab7ff7a47
quynxkba1 <quynx@twendeesoft.com>
2024-03-17 01:39:14 +0700

    feat: nft contract

--- src/AdvertiseXNFT.ts
+++ src/AdvertiseXNFT.ts
@@ -1,142 +1,36 @@
-export { MinaNFTContract };
+
+import {MinaNFT} from 'minanft';
 import {
-  Field,
-  state,
-  State,
-  method,
-  SmartContract,
-  UInt64,
-  Signature,
-  PublicKey,
-  Poseidon,
-} from "o1js";
-import { Update, Metadata, Storage } from "./libs/metadata";
-import { MinaNFTMetadataUpdateProof } from "./libs/update";
-import { EscrowTransfer, EscrowApproval } from "./libs/escrow";
+    PublicKey,
+    Field,
+    UInt64,
+  } from "o1js";
 
 /**
- * MinaNFTContract is a smart contract that implements the Mina NFT standard.
- * @property name The name of the NFT.
- * @property metadata The metadata of the NFT.
- * @property storage The storage of the NFT - IPFS (i:...) or Arweave (a:...) hash string
- * @property owner The owner of the NFT - Poseidon hash of owner's public key
- * @property escrow The escrow of the NFT - Poseidon hash of three escrow's public keys
- * @property version The version of the NFT, increases by one with the changing of the metadata or the owner
+ * This contract implement non-fungible token on Mina
  */
-class MinaNFTContract extends SmartContract {
-  @state(Field) name = State<Field>();
-  @state(Metadata) metadata = State<Metadata>();
-  @state(Storage) storage = State<Storage>();
-  @state(Field) owner = State<Field>();
-  @state(Field) escrow = State<Field>();
-  @state(UInt64) version = State<UInt64>();
-
-  /**
-   * Update metadata of the NFT
-   * @param update {@link Update} - data for the update
-   * @param signature signature of the owner
-   * @param owner owner's public key
-   * @param proof {@link MinaNFTMetadataUpdateProof} - proof of the update of the metadata to be correctly inserted into the Merkle Map
-   */
-  @method update(
-    update: Update,
-    signature: Signature,
-    owner: PublicKey,
-    proof: MinaNFTMetadataUpdateProof
-  ) {
-    // Check that the metadata is correct
-    const metadata = this.metadata.getAndAssertEquals();
-    Metadata.assertEquals(metadata, update.oldRoot);
-    Metadata.assertEquals(metadata, proof.publicInput.oldRoot);
-    Metadata.assertEquals(proof.publicInput.newRoot, update.newRoot);
-
-    // Check that the proof verifies
-    proof.verify();
-
-    signature.verify(owner, update.toFields()).assertEquals(true);
-    update.owner.assertEquals(Poseidon.hash(owner.toFields()));
-
-    this.owner
-      .getAndAssertEquals()
-      .assertEquals(update.owner, "Owner mismatch");
-    this.name.getAndAssertEquals().assertEquals(update.name, "Name mismatch");
-
-    const version = this.version.getAndAssertEquals();
-    const newVersion: UInt64 = version.add(UInt64.from(1));
-    newVersion.assertEquals(update.version);
-
-    this.metadata.set(update.newRoot);
-    this.version.set(newVersion);
-    this.storage.set(update.storage);
+export class AdvertiseXNFT extends MinaNFT {
+  name: string;
+  creator: string;
+  storage: string;
+  owner: Field;
+  escrow: Field;
+  version: UInt64;
+  isMinted: boolean;
+  address: PublicKey;
+  tokenId: Field | undefined;
+  nameService: PublicKey | undefined;
+  constructor(params: {
+    name: string;
+    address: PublicKey;
+    creator?: string;
+    storage?: string;
+    owner?: Field;
+    escrow?: Field;
+    nameService?: PublicKey;
+  }) {
+    super({name: params.name, address: params.address});
   }
-  /**
-   * Transfer the NFT to new owner
-   * @param data {@link EscrowTransfer} - data for the transfer
-   * @param signature1 signature of the first escrow
-   * @param signature2 signature of the second escrow
-   * @param signature3 signature of the third escrow
-   * @param escrow1 public key of the first escrow
-   * @param escrow2 public key of the second escrow
-   * @param escrow3 public key of the third escrow
-   */
-  @method transfer(
-    data: EscrowTransfer,
-    signature1: Signature,
-    signature2: Signature,
-    signature3: Signature,
-    escrow1: PublicKey,
-    escrow2: PublicKey,
-    escrow3: PublicKey
-  ) {
-    this.owner
-      .getAndAssertEquals()
-      .assertEquals(data.oldOwner, "Owner mismatch");
-    this.escrow
-      .getAndAssertEquals()
-      .assertNotEquals(Field(0), "Escrow is not set");
-    this.escrow.assertEquals(data.escrow);
-    this.name.getAndAssertEquals().assertEquals(data.name, "Name mismatch");
-    const version = this.version.getAndAssertEquals();
-    const newVersion: UInt64 = version.add(UInt64.from(1));
-    newVersion.assertEquals(data.version);
-    const dataFields = data.toFields();
-    signature1.verify(escrow1, dataFields).assertEquals(true);
-    signature2.verify(escrow2, dataFields).assertEquals(true);
-    signature3.verify(escrow3, dataFields).assertEquals(true);
-    data.escrow.assertEquals(
-      Poseidon.hash([
-        Poseidon.hash(escrow1.toFields()),
-        Poseidon.hash(escrow2.toFields()),
-        Poseidon.hash(escrow3.toFields()),
-      ])
-    );
-
-    this.owner.set(data.newOwner);
-    this.version.set(newVersion);
-    this.escrow.set(Field(0));
-  }
-  /**
-   * Approve setting of the new escrow
-   * @param data {@link EscrowApproval} - data for the approval
-   * @param signature signature of the owner
-   * @param owner owner's public key
-   */
-  @method approveEscrow(
-    data: EscrowApproval,
-    signature: Signature,
-    owner: PublicKey
-  ) {
-    signature.verify(owner, data.toFields()).assertEquals(true);
-    data.owner.assertEquals(Poseidon.hash(owner.toFields()));
-
-    this.owner.getAndAssertEquals().assertEquals(data.owner, "Owner mismatch");
-    this.name.getAndAssertEquals().assertEquals(data.name, "Name mismatch");
-
-    const version = this.version.getAndAssertEquals();
-    const newVersion: UInt64 = version.add(UInt64.from(1));
-    newVersion.assertEquals(data.version);
-
-    this.version.set(newVersion);
-    this.escrow.set(data.escrow);
-  }
+  
 }
+  
--- src/AdvertiseXToken.ts
+++ src/AdvertiseXToken.ts
@@ -11,6 +11,9 @@ import {
     Field,
   } from 'o1js';
   
+  /**
+   * This contract implememt fungible token on Mina network
+   */
   export class AdvertiseXToken extends SmartContract {
     @state(Field) symbol = State<Field>();
     @state(UInt64) decimals = State<UInt64>();
@@ -37,7 +40,12 @@ import {
     onlyOwner() {
       this.owner.getAndAssertEquals().assertEquals(this.sender);
     }
-  
+    
+    /**
+     * Mint token to account receiver , only admin can mint 
+     * @param receiverAddress Account recieve AdvertiseXToken
+     * @param amount Amount mint 
+     */
     @method mint(receiverAddress: PublicKey, amount: UInt64) {
       this.onlyOwner();
       const maxSupply = this.maxSupply.getAndAssertEquals();
@@ -55,7 +63,12 @@ import {
   
       this.totalAmountInCirculation.set(newCirculatingSupply);
     }
-  
+    
+     /**
+     * Burn token to account receiver , only admin can burn 
+     * @param burnerAddress Account burn AdvertiseXToken
+     * @param amount Amount burn 
+     */
     @method burn(burnerAddress: PublicKey, amount: UInt64) {
       this.onlyOwner();
       const maxSupply = this.maxSupply.getAndAssertEquals();
@@ -71,7 +84,13 @@ import {
   
       this.totalAmountInCirculation.set(newCirculatingSupply);
     }
-  
+    
+    /**
+     * Transfer token tfrom sender account to receiver account
+     * @param senderAddress Sender account 
+     * @param receiverAddress Receiver account
+     * @param amount Amount transfer 
+     */
     @method transfer(
       senderAddress: PublicKey,
       receiverAddress: PublicKey,
@@ -79,7 +98,11 @@ import {
     ) {
       this.token.send({ from: senderAddress, to: receiverAddress, amount });
     }
-  
+    
+    /**
+     * Get current balance
+     * @param address Account to get balance
+     */
     @method getBalance(address: PublicKey): UInt64 {
       let accountUpdate = AccountUpdate.create(address, this.token.id);
       let balance = accountUpdate.account.balance.get();
